4c6172e2c2b93ae38a4009cde969fddc3906f10b
Allenliuyanming <104706275+Allenliuyanming@users.noreply.github.com>
2022-05-01 12:08:39 +0800

    Add files via upload

---
+++ HelloWorld3.java
@@ -0,0 +1,19 @@
+/**
+文档注释
+@author zhaoss
+@version 1.0
+这是我们第一章文档注释的代码，比较重要
+*/
+public class HelloWorld3{
+	public static void main(String[] args){	
+		System.out.println("hi....java1");	
+	}
+	/**
+	@param name 姓名
+	@param age 年龄
+	*/
+	public void eat(String name,int age){
+		System.out.println("hello");	
+	}
+}
+
---
+++ HiWorld.java
@@ -0,0 +1,23 @@
+public class public{
+	public static void main(String[] args){
+		//进行自我介绍：
+		System.out.print("姓名：");
+		System.out.print("\t丽丽\n");
+		System.out.print("职业：");
+		System.out.print("\t学生");
+		/*
+		(1)System.out.print和System.out.println区别联系：
+		System.out.print ： 将双引号中内容原样输出，不换行
+		System.out.println ：将双引号中内容原样输出，换行
+		(2)转义字符：
+		\就是转义字符：作用：将后面普通的字母转换为特殊含义
+		\n  : 换行
+		\t  : 距离前面有一个制表符位置
+		*/
+		
+		System.out.println();//换行
+		System.out.println("1111111111111111111");
+		System.out.println("11111111\t2222");
+		int age ;
+	}
+}
